f3a0e9b9d325978c4df878ee13badf1d51396376
the_ATM <the_atm@wp.pl>
2021-10-07 00:00:45 +0200

    More assesments

--- VisonNet/Network/assesment.py
+++ VisonNet/Network/assesment.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image,  ImageTk
+
 import torch
 import torchvision
 import tkinter as tk
@@ -8,11 +9,15 @@ from torchvision import transforms
 from skimage import io
 from PIL import Image, ImageTk
 #Prove of concept
+import bankset as bank
+import parameters as par
+import model as mod
 
 asses_loader = None
 asses_data_path = ''
 
-my_image_path = ''
+MODEL_PATH = '../Models/OrgResnet18_11-09-2021_17-24_Epoch_0060_Acc_17.62.pth'
+my_image_path = '../../../Data/testset/500/63.jpg'
 
 transform_asses = transforms.Compose([
     transforms.ToPILImage(),
@@ -28,18 +33,54 @@ picture = None
 label = None
 
 def main():
-    root = tk.Tk()
-    button = tk.Button(root, text="Hejo", command=assesMyImage)
-    #picture = tk.PhotoImage( Image.open('../../../Data/testset/200/49.jpg'))
-    #picture = tk.PhotoImage(file='chocolate-bar.png')
-    img = Image.open('49.jpg')
-    #img.save('fafa.png')
-    picture = ImageTk.PhotoImage(img)
-    label = tk.Label(root,image=picture)
-    label.image = picture
-    label.pack()
-    button.pack()
-    root.mainloop()
+    print("Welcome to the assesment program")
+    print("You can check how the model is doing in action")
+
+    _, _, asses_loader = bank.loadData(arg_load_train=False, arg_load_val=False, arg_load_test=True)
+    asses_device = torch.device(par.TRAIN_ARCH)
+    if par.TRAIN_ARCH == 'cuda:0': torch.cuda.empty_cache()
+    used_model = mod.UsedModel('Original_Resnet18', loadPath=MODEL_PATH, load=True)
+    used_model.model.eval()
+    print("Assesment Started")
+
+    with torch.no_grad():
+        for i, batch in enumerate(asses_loader):
+            input_batch, label_batch, name_batch = batch['image'], batch['class'], batch['name']
+            output_batch = used_model.model(input_batch)
+            _, pred_batch = output_batch.topk(1, 1, True, True)
+            #pred_batch = pred_batch.t()
+            for j, data in enumerate(batch):
+                input = input_batch.numpy()[j]
+                label =label_batch.numpy()[j]
+                name = name_batch[j]
+                pred = pred_batch.numpy()[j][0]
+                output = output_batch.numpy()[j]
+
+                user_choice = assesMenu()
+                if user_choice == 0: continue
+                elif user_choice == 1: return
+                #elif user_choice == 2:
+                    #show image
+
+            correct = pred.eq(label.contiguous().view(1, -1).expand_as(pred))
+            correct_k = correct[:1].reshape(-1).float().sum(0, keepdim=True)
+            print(str(correct_k))
+
+
+    img = Image.open(my_image_path)
+    img.show()
+
+
+
+
+def assesMenu():
+    print("To exit press q")
+    print("To see picture press s")
+    print("To continue press anything else")
+    user_choice = input()
+    if user_choice == 'q': return 1
+    elif user_choice == 's': return 2
+    return 0
 
 def assesMyImage():
     image = io.imread(my_image_path)
